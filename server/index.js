ed4087410d16b33b7613f460ff611e9da5392248
The1WhoKnocks <97444040+DhruvHeisenberg@users.noreply.github.com>
2024-03-21 04:22:27 +0530

    Update cors with frontend deployed url

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.use(cors({
-    origin: [""],
+    origin: ["https://share-sync-frontend.vercel.app/"],
     methods: ["POST", "GET", "PUT"],
     credentials:true
 }))
